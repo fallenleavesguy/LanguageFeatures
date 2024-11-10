ace344e46af15b7063dd9a06bfa2664a03a76888
fallenleaves256 <9261562+fallenleavesguy@users.noreply.github.com>
2024-11-10 10:35:09 +0800

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+           var products = new[]
+           {
+               new { Name = "Kayak", Price = 275M },
+               new { Name = "Lifejacket", Price = 48.95M },
+               new { Name = "Soccer ball", Price = 19.50M },
+               new { Name = "Corner flag", Price = 34.95M }
+           };
+
+            return View(products.Select(p => p.GetType().Name));
+        }
+    }
+}
